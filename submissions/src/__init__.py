93034f2bae36c866408460c86c71d850143d1fc3
Chaitanya-666 <161066628+Chaitanya-666@users.noreply.github.com>
2025-11-21 22:06:36 +0530

    fix3

--- submissions/src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-OT Project: Municipal Waste Collection with Intermediate Facilities
-ALNS (Adaptive Large Neighborhood Search) Implementation
-
-Authors:
-- Harsh Sharma (231070064) - Algorithm Development
-- Chaitanya Shinde (231070066) - System Architecture
-"""
